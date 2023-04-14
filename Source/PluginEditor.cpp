4b28342c13fc9e483f0c5dd36cab251abae8419e
WhySoFF <mrstingerhac@gmail.com>
2023-04-15 02:01:41 +0300

    FM_LABELS_AND_FIX

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,7 +11,7 @@
 
 //==============================================================================
 ShelSynthAudioProcessorEditor::ShelSynthAudioProcessorEditor (ShelSynthAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p), osc (audioProcessor.apvts, "OSC1WAVETYPE"), adsr (audioProcessor.apvts)
+    : AudioProcessorEditor (&p), audioProcessor (p), osc (audioProcessor.apvts, "OSC1WAVETYPE", "OSC1FMFREQ", "OSC1FMDEPTH"), adsr (audioProcessor.apvts)
 {
     setSize (400, 300);
     addAndMakeVisible(osc);
@@ -26,12 +26,15 @@ ShelSynthAudioProcessorEditor::~ShelSynthAudioProcessorEditor()
 //==============================================================================
 void ShelSynthAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (juce::Colours::darkgrey); 
+    g.fillAll (juce::Colours::darkgrey);
+    g.setColour(juce::Colours::white);
+    g.drawRect(getLocalBounds());
+    
 }
 
 void ShelSynthAudioProcessorEditor::resized()
 {
-    osc.setBounds(10, 10, 100, 30);
+    osc.setBounds(10, 10, 180, 200);
     adsr.setBounds(getWidth() / 2, 0, getWidth() / 2, getHeight());
 }
 
--- Source/UI/OscComponent.cpp
+++ Source/UI/OscComponent.cpp
@@ -12,13 +12,16 @@
 #include "OscComponent.h"
 
 //==============================================================================
-OscComponent::OscComponent(juce::AudioProcessorValueTreeState& apvts, juce::String waveSelectorId)
+OscComponent::OscComponent(juce::AudioProcessorValueTreeState& apvts, juce::String waveSelectorId, juce::String fmFreqId, juce::String fmDepthId)
 {
     juce::StringArray choices = { "Sine", "Saw", "Square" };
     oscWaveSelector.addItemList(choices, 1);
     addAndMakeVisible(oscWaveSelector);
     
     oscWaveSelectorAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(apvts, waveSelectorId, oscWaveSelector);
+    
+    setSliderWithLabel(fmFreqSlider, fmFreqLabel, apvts, fmFreqId, fmFreqAttachment);
+    setSliderWithLabel(fmDepthSlider, fmDepthLabel, apvts, fmDepthId, fmDepthAttachment);
 
 }
 
@@ -33,6 +36,36 @@ void OscComponent::paint (juce::Graphics& g)
 
 void OscComponent::resized()
 {
+    const auto sliderPosY = 80;
+    const auto sliderWidth = 100;
+    const auto sliderHeight = 90;
+    const auto labelYOffset = 20;
+    const auto labelHeight = 20;
+    
     oscWaveSelector.setBounds(0, 0, 90, 20);
+    
+    fmFreqSlider.setBounds(0, sliderPosY, sliderWidth, sliderHeight);
+    fmFreqLabel.setBounds(fmFreqSlider.getX(), fmFreqSlider.getY() - labelYOffset, fmFreqSlider.getWidth(), labelHeight);
+    
+    fmDepthSlider.setBounds(fmFreqSlider.getRight(), sliderPosY, sliderWidth, sliderHeight);
+    fmDepthLabel.setBounds(fmDepthSlider.getX(), fmDepthSlider.getY() - labelYOffset, fmDepthSlider.getWidth(), labelHeight);
 
+}
+
+using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
+//slider creator and labeler
+void OscComponent::setSliderWithLabel (juce::Slider& slider, juce::Label& label, juce::AudioProcessorValueTreeState& apvts, juce::String paramId, std::unique_ptr<Attachment>& attachment)
+{
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 25);
+    addAndMakeVisible(slider);
+    
+    attachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, paramId, slider);
+    
+    //Labeling of Rotator sliders
+    label.setColour(juce::Label::ColourIds::textColourId, juce::Colours::white);
+    label.setFont(15.0f);
+    label.setJustificationType(juce::Justification::centred);
+    addAndMakeVisible(label);
 }
